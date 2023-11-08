6c38253b510098a2e47d03621b7f31342cb714a2
adindravickarega <adindravickar@gmail.com>
2023-11-09 05:22:48 +0700

    pass say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -38,7 +38,7 @@ describe('HTTP server', () => {
         method: 'GET',
         url: '/',
       });
-      // Assert
+      //Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
       expect(responseJson.value).toEqual('Hello world!');
